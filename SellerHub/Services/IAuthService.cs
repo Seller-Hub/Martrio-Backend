10ff1a12083de8f6cb1b28433f57f8770c8e2c45
Selim <sesaridede3@gmail.com>
2025-09-14 11:37:49 -0700

    AuthController update

--- SellerHub/Services/IAuthService.cs
+++ SellerHub/Services/IAuthService.cs
@@ -8,13 +8,13 @@ namespace SellerHub.Services
         // CUSTOMER
         Task<User?> RegisterCustomerAsync(RegisterCustomerDto dto);
 
-        // SELLER
-        Task<User?> RegisterSellerStep1Async(RegisterSellerStep1Dto dto);
-        Task<User?> RegisterSellerStep2Async(int userId, RegisterSellerStep2Dto dto);
+        // // SELLER
+        // Task<User?> RegisterSellerStep1Async(RegisterSellerStep1Dto dto);
+        // Task<User?> RegisterSellerStep2Async(int userId, RegisterSellerStep2Dto dto);
 
-        // ADMIN
-        Task<User?> RegisterAdminStep1Async(RegisterAdminStep1Dto dto);
-        Task<User?> RegisterAdminStep2Async(int userId, RegisterAdminStep2Dto dto);
+        // // ADMIN
+        // Task<User?> RegisterAdminStep1Async(RegisterAdminStep1Dto dto);
+        // Task<User?> RegisterAdminStep2Async(int userId, RegisterAdminStep2Dto dto);
 
         // LOGIN
         Task<User?> LoginAsync(LoginDto dto);
